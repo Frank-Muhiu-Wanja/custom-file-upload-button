2efff6db5642c749f695b236c7bd16057c591103
Frank_Onyx <frankmuhiu648@gmail.com>
2025-09-28 21:02:18 +0300

    Added files "skadush!!!"

---
+++ script.js
@@ -0,0 +1,33 @@
+const music = document.getElementById("bg-music");
+const playBtn = document.getElementById("playBtn");
+
+playBtn.addEventListener("click", () => {
+  if (music.paused) {
+    music.play();
+    playBtn.textContent = "Pause Music";
+  } else {
+    music.pause();
+    playBtn.textContent = "Play Music";
+  }
+});
+
+document.querySelectorAll(".file-upload__button").forEach((button) => {
+  const hiddenInput = button.parentElement.querySelector(".file-upload__input");
+  const label = button.parentElement.querySelector(".file-upload__label");
+  const defaultLabelText = "No file(s) selected";
+
+  // Set default text for label
+  label.textContent = defaultLabelText;
+  label.title = defaultLabelText;
+
+  button.addEventListener("click", function () {
+    hiddenInput.click();
+  });
+  hiddenInput.addEventListener("change", function () {
+    const filenameList = Array.from(hiddenInput.files).map(function (file) {
+      return file.name;
+    });
+    label.textContent = filenameList.join(", ") || defaultLabelText;
+    label.title = label.textContent;
+  });
+});
